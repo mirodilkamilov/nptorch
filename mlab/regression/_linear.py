6d58b65269110d30f6b69f2c88d4d7c94c742bea
Mirodil Kamilov <mirodilkamilov1999@gmail.com>
2026-04-26 17:33:40 +0200

    Add Stochastic Gradient Descent implementation

--- mlab/regression/_linear.py
+++ mlab/regression/_linear.py
@@ -1,4 +1,7 @@
+import math
+
 import numpy as np
+from numpy import dtype
 from numpy.typing import NDArray
 
 
@@ -34,8 +37,8 @@ class LinearRegressor:
         # Use instead Singular Value Decomposition approach - SVD splits any matrix X into three pieces X = U · Σ · Vᵀ
         # where V rotates the input space, Σ stretches/shrinks along each axis, U rotates the output space.
         # Rotation doesn't change the length of the vector, therefore no introduction of numerical error. Σ might introduce one.
-        # Then calculate w = (Xᵀ·X)⁻¹·Xᵀ·y where X = U·Σ·Vᵀ, which becomes simply w = V · Σ⁺ · Uᵀ · y.
-        # To invert Σ, zero out nearly zero values (0.0001 ≈ 0) -> pseudo-inverse Σ⁺.
+        # Then calculate w = (Xᵀ·X)⁻¹·Xᵀ·y where X = U·Σ·Vᵀ, which becomes simply w = V · Σ⁺ · Uᵀ · y
+        # To invert Σ, zero out nearly zero values (0.0001 ≈ 0) to avoid numerically instability -> pseudo-inverse Σ⁺.
         self.weights_, _, _, _ = np.linalg.lstsq(X_, y, rcond=None)
         return self
 
@@ -57,11 +60,15 @@ class LinearRegressor:
 
 
 class SGDRegression:
-    def __init__(self, learning_rate=0.01, n_iterations=1000, batch_size=32):
+    def __init__(self, learning_rate=0.01, epochs=1000, batch_size=32):
         self.weights_ = None
         self.bias_ = None
 
-    def fit(self, X, y):
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.batch_size = batch_size
+
+    def fit(self, X: NDArray[np.floating], y: NDArray[np.floating]):
         """
         Train the model using stochastic gradient descent.
 
@@ -69,9 +76,44 @@ class SGDRegression:
             X: numpy array of shape (n_samples, n_features)
             y: numpy array of shape (n_samples,)
         """
-        pass
+        if self.weights_ is None and self.bias_ is None:
+            self.weights_ = np.zeros((X.shape[1], 1), dtype=X.dtype)
+            self.bias_ = np.zeros((1, 1), dtype=X.dtype)
 
-    def predict(self, X):
+        rng = np.random.default_rng(seed=42)
+
+        epoch = 1
+        while self.epochs >= epoch:
+            indices = rng.permutation(X.shape[0])
+            X_shuffled = X[indices]
+            y_shuffled = y[indices]
+
+            num_batches = math.ceil(X_shuffled.shape[0] / self.batch_size)
+            batches = np.array_split(X_shuffled, num_batches)
+            y_batches = np.array_split(y_shuffled, num_batches)
+
+            epoch_loss = 0
+            for X_batch, y_batch in zip(batches, y_batches):
+                y_hat = self._predict(X_batch)
+                # Make same shape as y_hat: (32,) -> (32,1)
+                y_batch = y_batch.reshape(-1, 1)
+
+                epoch_loss += self._mse(y_batch, y_hat)
+
+                grad = self._mse_grad(y_batch, y_hat)
+                self.weights_ = self.weights_ - self.learning_rate * (X_batch.T @ grad)
+                self.bias_ = self.bias_ - self.learning_rate * np.mean(grad)
+
+            print(f"Epoch {epoch}/{self.epochs} | Loss: {epoch_loss / num_batches:.4f}")
+            epoch += 1
+
+        return self
+
+    def _predict(self, X: NDArray[np.floating]) -> NDArray[np.floating]:
+        """Returns predictions in 2D shape (n_samples, 1) for internal use."""
+        return X @ self.weights_ + self.bias_
+
+    def predict(self, X: NDArray[np.floating]):
         """
         Predict target values for the given input.
 
@@ -81,4 +123,10 @@ class SGDRegression:
         Returns:
             numpy array of shape (n_samples,)
         """
-        pass
+        return (X @ self.weights_ + self.bias_).flatten()
+
+    def _mse(self, y, y_hat):
+        return np.mean((y_hat - y) ** 2)
+
+    def _mse_grad(self, y, y_hat):
+        return 2 * (y_hat - y) / len(y)
