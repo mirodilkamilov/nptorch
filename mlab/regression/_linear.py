2ac1db80044029f2e451d8641465a8670279eaef
Mirodil Kamilov <mirodilkamilov1999@gmail.com>
2026-04-26 13:08:31 +0200

    Add more numerically stable linear regression implementation (uses np.linalg.lstsq instead of inv)

--- mlab/regression/_linear.py
+++ mlab/regression/_linear.py
@@ -6,8 +6,16 @@ class LinearRegressor:
     def __init__(self):
         self.weights_ = None
 
+    @property
+    def intercept_(self):
+        return self.weights_[0]
+
+    @property
+    def coef_(self):
+        return self.weights_[1:]
+
     def _add_dummy_feature(self, X: NDArray[np.floating]) -> NDArray[np.floating]:
-        dummy_feature = np.ones((X.shape[0], 1), dtype=np.float64)
+        dummy_feature = np.ones((X.shape[0], 1), dtype=X.dtype)
         return np.concatenate((dummy_feature, X), axis=1)
 
     def fit(self, X: NDArray[np.floating], y: NDArray[np.floating]):
@@ -19,10 +27,17 @@ class LinearRegressor:
             y: numpy array of shape (n_samples,)
         """
         X_ = self._add_dummy_feature(X)
-        self.weights_ = np.linalg.inv(X_.T @ X_) @ X_.T @ y
+
+        # Computing matrix inverse is numerically unstable: self.weights_ = np.linalg.inv(X_.T @ X_) @ X_.T @ y
+        # Use instead Singular Value Decomposition approach - SVD splits any matrix X into three pieces X = U · Σ · Vᵀ
+        # where V rotates the input space, Σ stretches/shrinks along each axis, U rotates the output space.
+        # Rotation doesn't change the length of the vector, therefore no introduction of numerical error. Σ might introduce one.
+        # Then calculate w = (Xᵀ·X)⁻¹·Xᵀ·y where X = U·Σ·Vᵀ, which becomes simply w = V · Σ⁺ · Uᵀ · y.
+        # To invert Σ, zero out nearly zero values (0.0001 ≈ 0) -> pseudo-inverse Σ⁺.
+        self.weights_, _, _, _ = np.linalg.lstsq(X_, y, rcond=None)
         return self
 
-    def predict(self, X):
+    def predict(self, X: NDArray[np.floating]):
         """
         Predict target values for the given input.
 
@@ -65,32 +80,3 @@ class SGDRegression:
             numpy array of shape (n_samples,)
         """
         pass
-
-# def main() -> None:
-#     X = np.array(
-#         [
-#             [4.0, 100],
-#             [5.0, 110],
-#             [5.5, 105],
-#             [6.0, 120],
-#             [6.5, 115],
-#             [7.0, 125],
-#             [7.5, 130],
-#             [8.0, 135],
-#             [8.5, 130],
-#             [9.0, 140],
-#         ],
-#         dtype=np.float64,
-#     )
-#     y = np.array([10, 12, 13, 15, 16, 18, 19, 21, 22, 24], dtype=np.float64)
-#
-#     model = LinearRegressor()
-#     model.fit(X, y)
-#     print(model.weights_)
-#
-#     prediction = model.predict(np.array([[7.3, 130]]))
-#     print(prediction)
-#
-#
-# if __name__ == "__main__":
-#     main()
